2160dbec4c64c4ed95dcfdb3032bf7545aafc707
Nico <info@ni-wa.de>
2025-08-28 21:35:00 +0000

    fix(prisma): migrations and prettify schema by prisma cli

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,4 @@
+-- This is an empty migration.
+
+-- The database tables are expected to already exist, as they are created or imported by the user  due to the specific freeradius schema.
+-- The only other solution would be to provide a database schema and configure this schema in freeradius, which is probably more work, as freeradius installations are expected to be already installed. If not, this would a solution.
